8d557d61bd11e09c2d49de7447492a460151d4d4
Illya Moskvin <imoskvin@artic.edu>
2022-03-01 15:24:56 -0600

    Make API's `AbstractTransformer` actually abstract [API-247]

--- app/Transformers/Outbound/Api/AbstractTransformer.php
+++ app/Transformers/Outbound/Api/AbstractTransformer.php
@@ -5,7 +5,7 @@ namespace App\Transformers\Outbound\Api;
 use App\Transformers\Outbound\Concerns\HasDates;
 use Aic\Hub\Foundation\AbstractTransformer as BaseTransformer;
 
-class AbstractTransformer extends BaseTransformer
+abstract class AbstractTransformer extends BaseTransformer
 {
     use HasDates;
 }
