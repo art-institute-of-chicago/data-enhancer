afc7286e68bbb4a50ee75d25bac2e68a2832650b
nikhiltri <ntrivedi@artic.edu>
2026-09-16 09:06:58 -0500

    Lint code [WEB-3447]

--- config/database.php
+++ config/database.php
@@ -149,7 +149,7 @@ $config = [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')) . '-database-'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
